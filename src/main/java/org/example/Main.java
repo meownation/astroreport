8e9f26f6797d183067c6ba4d9d0ecbf3220345b1
meownation <meownation@protonmail.com>
2022-06-10 15:53:41 +0200

    initial

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,5 @@
 package org.example;
 
-import org.testcontainers.shaded.org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -9,8 +8,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.testcontainers.shaded.org.apache.commons.io.FileUtils;
 
 import java.io.File;
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Calendar;
 
 @SpringBootApplication
 public class Main {
@@ -27,27 +32,43 @@ public class Main {
         return builder.build();
     }
 
+    public static String addDay(String date) throws ParseException {
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-mm-dd");
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(sdf.parse(date));
+        calendar.add(Calendar.DATE, 1);  // number of days to add to your existing date
+        return sdf.format(calendar.getTime());
+
+    }
+
 
  //   @Scheduled(fixedRate = 3000)
 
     @Bean
     public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
         return args -> {
-            File f = new File("target/test.txt");
-            f.delete();
-            DailyHoroscope dailyHoroscope = null;
-            for (AstroSign sign :AstroSign.values()){
+            LocalDate date= LocalDate.now();
+            String sqldate =Date.valueOf(date).toString();
+            for(int i=0;i<10;i++) {
+                File f = new File(String.format("target/%s.txt", sqldate));
+                sqldate= addDay(sqldate);
 
-                dailyHoroscope =
-                    restTemplate.getForObject(
-                    "http://localhost:8080/daily/{sign}", DailyHoroscope.class, sign);
+                f.delete();
+                DailyHoroscope dailyHoroscope = null;
+                for (AstroSign sign : AstroSign.values()) {
 
-                FileUtils.writeStringToFile(f, dailyHoroscope.toString(),"UTF-8", true);
-            log.info(dailyHoroscope.toString());
-            System.out.println(dailyHoroscope);}
+                    dailyHoroscope =
+                            restTemplate.getForObject(
+                                    "http://localhost:8080/daily/{sign}?date={sqldate}", DailyHoroscope.class, sign, sqldate);
+
+                    FileUtils.writeStringToFile(f, dailyHoroscope.toString(), "UTF-8", true);
+                    log.info(dailyHoroscope.toString());
+                    System.out.println(dailyHoroscope);
+                }
 
 
-
+            }
         };
     }
 
